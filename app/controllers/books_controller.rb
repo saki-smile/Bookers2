360e184ca8a1b755d3f0a9fd9d9d72b0219623a9
saki-smile <sa.su.ko0419@gmail.com>
2020-11-14 11:48:04 +0000

    [Add] bookmodel

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def create
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def destoroy
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
+  resources :users, only: [:show, :index, :edit]
 end
