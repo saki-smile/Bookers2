707ca414ee40ddfe6aed96191e869fd9f2565563
saki-smile <sa.su.ko0419@gmail.com>
2020-11-15 13:07:54 +0000

    [Add] userinfo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,43 @@
 class BooksController < ApplicationController
-  def create
+  def new
+    @book = Book.new
   end
   
+  def create
+    @book = Book.new(book_parames)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
-  def destoroy
-    
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_parames)
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_parames
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books
 end
